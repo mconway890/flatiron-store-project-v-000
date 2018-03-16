fa651762d3d92d6939437b13ccbdbb4021a0fc28
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2018-03-16 18:48:32 +0000

    first commit

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,13 @@
 class Cart < ActiveRecord::Base
-
+    belongs_to :user
+    has_many :line_items
+    has_many :items, through: :line_items
+    
+    def total
+        
+    end
+    
+    def add_item
+        
+    end
 end
